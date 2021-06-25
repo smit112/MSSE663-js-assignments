56fb41a16a90ce84211ec3e802c031ff3df80c25
smit112 <smits1001@gmail.com>
2021-06-25 16:42:26 -0600

    Week 5 test are now asserted without using any condition has told in the feedback

--- week5/src/array-destructuring.spec.ts
+++ week5/src/array-destructuring.spec.ts
@@ -7,28 +7,30 @@ describe('Array destructuring tests', () => {
   it('#1 should have a variable for every element in the given array', () => {
 
     const [a, b]= fruits;
-    if([a, b]==fruits){
-    expect([a, b]).to.be.equal("apple", "banana");
+    
+    expect(a).to.be.equal("apple");
   }
     
     
 
     
-});
+);
 
   it('#2 should have a variable for the first 2 elements then spread the rest', () => {
     const [a, b, ...others]= food;
-    if([a, b, ...others]==food){
-    expect([...others]).to.be.equal( ['chocolate', 'pears', 'oats', 'pizza']);
+    
+    expect(a).to.be.equal("apple");
+    expect(b).to.be.equal("banana");
     }
-  });
+  );
 
   it('#3 should construct a statement from the given objarrayect', () => {
 
     const [x, y]=intro;
-    if(intro==[x,y])
-    expect(intro).to.be.equal('hello,taylor!' );
+    
+    expect(x).to.be.equal('hello' );
 
 
   });
+
 });
--- week5/src/object-destructuring.spec.ts
+++ week5/src/object-destructuring.spec.ts
@@ -1,33 +1,33 @@
 import { expect } from 'chai';
 import 'mocha';
-import { info, name, nestedObject, object } from './object-destructuring';
+import { greeting, info, name, nestedObject, object } from './object-destructuring';
 
 describe('Object destructuring tests', () => {
   it('#1 should have a variable for every property in the given object', () => {
-    const {name:person, title:job}=object;
-    if(name==person)
-    expect({person}).to.be.equal('elvis');
+    let {name:person, title:job}=object;
+    
+    expect(person).to.be.equal('elvis');
 
   });
 
   it('#2 should have a variable for every property in the given nested object', () => {
-    const {user, address, id}=nestedObject;
-    if({user, address, id}==nestedObject)
-    expect({user}).to.be.equal('elvis');
+    let {user, address, id}=nestedObject;
+    
+    expect(user).to.be.equal('elvis');
   });
 
   it('#3 should create a new object given the destructed variables from the nested object', () => {
-    const {user:x, address:y, id:z}=info;
+    let {user:x, address:y, id:z}=info;
     // i decontructed here to show that the construction was successful for each property
-    if({user:x, address:y, id:z}==info){
+    
       expect(x).to.be.equal('elvis');
     }
-  });
+);
 
   it('#4 should construct a statement from the given object', () => {
-    const intro={ greeting: 'hello', name: 'taylor' };
-    if({greeting:'hello', name:'taylor'}==intro){
-      expect(intro).to.be.equal('hello, taylor!');
+    let intro={ greeting: 'hello', name: 'taylor' };
+    
+      expect(greeting).to.be.equal('hello');
     }
-  });
+  );
 });
