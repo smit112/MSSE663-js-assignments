784bbf40c403fefea034a5c84a5e1ff466399a1d
smit112 <smits1001@gmail.com>
2021-06-27 17:21:39 -0600

    Revert "Deleted trying to fix the errors"
    This reverts commit a21a8d49b2023a5e4363b11d13eab49ec880914a.

---
+++ todo-Backend/todoapp/angular-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todo-Backend/todoapp/src/main/java/com/example/todoapp/models/NotBlank.java
@@ -0,0 +1,5 @@
+package com.example.todoapp.models;
+
+public @interface NotBlank {
+
+}
